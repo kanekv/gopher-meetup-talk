309b09f1c56c2bd8409cf6cf956aee75f49dd45d
Vasko Zdravevski <vasko.zdravevski@sendgrid.com>
2015-07-09 17:47:31 -0600

    add basics for linked lists

---
+++ code/linked_lists/linked_list_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"container/list"
+	"flag"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+// BEGIN OMIT
+func TestLargeLinkedList(t *testing.T) {
+	l := list.New()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 10e6; i++ {
+		l.PushBack(r.Int63())
+	}
+	runtime.GC()
+}
+
+func BenchmarkLinkedList(b *testing.B) {
+	l := list.New()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < b.N; i++ {
+		l.PushBack(r.Int63())
+	}
+}
+
+// END OMIT
+
+func main() {
+	flag.Set("test.bench", "Large")
+	flag.Set("test.v", "true")
+
+	testing.Main(func(pat, str string) (bool, error) { return true, nil },
+		[]testing.InternalTest{{"TestLargeLinkedList", TestLargeLinkedList}},
+		[]testing.InternalBenchmark{{"BenchmarkLinkedList", BenchmarkLinkedList}},
+		[]testing.InternalExample{})
+}
