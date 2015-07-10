27939352f74cfba0e5ae7b7a8e3f553078aa91be
Vasko Zdravevski <vasko.zdravevski@sendgrid.com>
2015-07-09 20:36:11 -0600

    2nd pipe example

---
+++ code/pipe2/pipe2.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"runtime"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+// SETUP_START OMIT
+var b = []byte(strings.Repeat("*", 1000))
+
+type CustomPipe struct {
+	buf bytes.Buffer
+}
+
+func (br *CustomPipe) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+func (br *CustomPipe) readmore() (n int, err error) {
+	return br.buf.Write(b)
+}
+func (br *CustomPipe) WriteTo(dst io.Writer) (n int64, err error) {
+	n = 0
+	for i := 0; i < 100e3; i++ {
+		br.readmore()
+		written, err := br.buf.WriteTo(dst)
+		if err != nil {
+			return 0, err
+		}
+		n += written
+	}
+	return n, err
+}
+
+// SETUP_END OMIT
+
+// BEGIN OMIT
+func TestPipe2(t *testing.T) {
+	start := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		br := &CustomPipe{}
+		buf := &bytes.Buffer{}
+		io.Copy(buf, br)
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println("TOTAL time Pipe2: ", time.Since(start)/1e6)
+}
+
+// END OMIT
+
+func main() {
+	fmt.Println(runtime.Version())
+	flag.Set("test.bench", "foo")
+	flag.Set("test.v", "true")
+
+	testing.Main(func(pat, str string) (bool, error) { return true, nil },
+		[]testing.InternalTest{{"TestPipe2", TestPipe2}},
+		[]testing.InternalBenchmark{},
+		[]testing.InternalExample{})
+}
